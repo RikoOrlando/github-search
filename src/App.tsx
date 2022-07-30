7647ca39791c554f1bd7a29b433c8497097f1e7a
Riko Orlando <rikoorlando76@gmail.com>
2022-07-30 08:44:03 +0700

    feat: setup pages, types, vscode settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { IRoute } from "./types/route";
+import routes from "pages";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={"Loading..."}>
+        <Routes>
+          {routes.map(({ path, Component }: IRoute) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/index.ts
@@ -0,0 +1,12 @@
+import { lazy } from "react";
+import { IRoute } from "types/route";
+
+const Home = lazy(() => import(/* webpackChunkName: "home-Page" */ "./home"));
+const routes: IRoute[] = [
+  {
+    Component: Home,
+    path: "/",
+  },
+];
+
+export default routes;
